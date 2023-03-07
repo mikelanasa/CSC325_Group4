ce5fffea0d1e7e8b5f3d724f4d2a0c7c8dea9403
erickxcruz <85849149+erickxcruz@users.noreply.github.com>
2023-03-07 16:04:24 -0500

    Added Comments to the Customer Class / Modified the default constructor

--- BakerySystem/src/main/java/com/mycompany/bakerysystem/Customer.java
+++ BakerySystem/src/main/java/com/mycompany/bakerysystem/Customer.java
@@ -9,16 +9,33 @@ package com.mycompany.bakerysystem;
  * @author erickcruz
  */
 public class Customer {
+    //private member variables that will hold customer information.
     private String firstName;
     private String lastName;
     private String email;
     private String phoneNumber;
     private String password;
     
+    /**
+     * This is the default constructor for the Customer class.
+     */
     public Customer(){
-        
-    }
+        this.firstName = null;
+        this.lastName = null;
+        this.email = null;
+        this.phoneNumber = null;
+        this.password = null;
 
+    }
+    
+    /**
+     * This is a constructor that takes in parameters for the customer information 
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param phoneNumber
+     * @param password 
+     */
     public Customer(String firstName, String lastName, String email, String phoneNumber, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -28,43 +45,81 @@ public class Customer {
     }
     
     
-
+    /**
+     * Getter method that returns the users First Name.
+     * @return 
+     */
     public String getFirstName() {
         return firstName;
     }
-
+    /**
+     * Setter method for the users first name 
+     * @param firstName 
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
+    
+    /**
+     * Getter method that returns the users Last Name.
+     * @return 
+     */
     public String getLastName() {
         return lastName;
     }
-
+    
+    /**
+     * Setter method for the users last name 
+     * @param lastName 
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
+    
+    /**
+     * Getter method that returns the users email.
+     * @return 
+     */
     public String getEmail() {
         return email;
     }
-
+    
+    /**
+     * Setter method for the users email  
+     * @param email 
+     */
     public void setEmail(String email) {
         this.email = email;
     }
-
+    
+    /**
+     * Getter method that returns the users phone number.
+     * @return 
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
+    
+    /**
+     * Setter method for the users phone number 
+     * @param phoneNumber 
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
+    
+    /**
+     * Getter method that returns the users password.
+     * @return 
+     */
     public String getPassword() {
         return password;
     }
-
+    
+    /**
+     * Setter method for the users password. 
+     * @param password 
+     */
     public void setPassword(String password) {
         this.password = password;
     }
