0f47bef5203ffbf5a32363e638ade24e88fc3508
erickxcruz <85849149+erickxcruz@users.noreply.github.com>
2023-03-23 10:20:26 -0400

    New scenes and controllers for pages

---
+++ BakerySystem/src/main/java/com/mycompany/bakerysystem/QuaternaryController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.bakerysystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author erickcruz
+ */
+public class QuaternaryController implements Initializable {
+    
+    @FXML 
+    private Button TEST;
+            
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
