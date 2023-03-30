f07548330dc0f2838e825af401cf69eed8e0c0a7
erickxcruz <85849149+erickxcruz@users.noreply.github.com>
2023-03-29 21:52:11 -0400

    Updated app info

--- BakerySystem/src/main/java/module-info.java
+++ BakerySystem/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.bakerysystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.bakerysystem to javafx.fxml;
     exports com.mycompany.bakerysystem;
