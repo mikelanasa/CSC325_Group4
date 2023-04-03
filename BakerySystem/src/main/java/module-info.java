91b179205cc992dd3049e8b1b22be317b7e71e1c
mikelanasa <mikelanasa@gmail.com>
2023-04-03 17:49:10 -0400

    removed firebase packages to remove error

--- BakerySystem/src/main/java/module-info.java
+++ BakerySystem/src/main/java/module-info.java
@@ -3,8 +3,6 @@ module com.mycompany.bakerysystem {
     requires javafx.fxml;
     requires java.base;
     
-    requires com.google.auth.oauth2;
-    requires firebase.admin;
             
     opens com.mycompany.bakerysystem to javafx.fxml;
     exports com.mycompany.bakerysystem;
