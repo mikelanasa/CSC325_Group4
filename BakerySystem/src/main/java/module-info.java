d00a70edb92e440af98fdad869c6579e7a537c95
mikelanasa <mikelanasa@gmail.com>
2023-04-02 20:52:14 -0400

    added FireBase packages to module-info

--- BakerySystem/src/main/java/module-info.java
+++ BakerySystem/src/main/java/module-info.java
@@ -2,7 +2,10 @@ module com.mycompany.bakerysystem {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
-
+    
+    requires com.google.auth.oauth2;
+    requires firebase.admin;
+            
     opens com.mycompany.bakerysystem to javafx.fxml;
     exports com.mycompany.bakerysystem;
 }
