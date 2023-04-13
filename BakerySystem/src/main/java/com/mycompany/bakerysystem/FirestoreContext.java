c18c4aab9b359937a5bded040e40728596c818bc
mikelanasa <mikelanasa@gmail.com>
2023-04-13 17:02:31 -0400

    Added FirestoreContext

---
+++ BakerySystem/src/main/java/com/mycompany/bakerysystem/FirestoreContext.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bakerysystem;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.auth.UserRecord.CreateRequest;
+import com.google.firebase.cloud.FirestoreClient;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mikel
+ */
+public class FirestoreContext {
+    
+    public Firestore firebase() {
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(getClass().getResourceAsStream("key.json")))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return FirestoreClient.getFirestore();
+    }
+    
+}
