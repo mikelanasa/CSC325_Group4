644db73319f9d380f915a18961bfc32433136167
ahmad <ahmad@DESKTOP-5CT1GNH>
2023-05-04 22:57:16 -0400

    Finish

--- BakerySystem/src/main/java/com/mycompany/bakerysystem/MenuController.java
+++ BakerySystem/src/main/java/com/mycompany/bakerysystem/MenuController.java
@@ -1,25 +1,66 @@
 package com.mycompany.bakerysystem;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 
 public class MenuController {
+
     @FXML
     private Button logOutButton;
+
+    @FXML
+    private GridPane MenuGrid;
+
     @FXML
     private Button shutDownButton;
+
+    @FXML
+    private Button AddtoOrder;
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("logInPage");
     }
-    
+
     @FXML
-    private void shutDownApp(){
+    private void shutDownApp() {
         Platform.exit();
     }
-    
-    
+
+    @FXML
+    void initialize() throws FileNotFoundException {
+        List<MenuItem> menuItems = new ArrayList<MenuItem>();
+
+       
+        for (int i = 0; i < 3; i++) {
+            Image image = new Image(getClass().getResourceAsStream("/com/mycompany/bakerysystem/bakerylogo.png"));
+            MenuItem NewItem = new MenuItem("Cake", 20.0, "sdsdsd", image);
+            menuItems.add(NewItem);
+        }
+        MenuDisplay(menuItems);
+
+    }
+
+    private void MenuDisplay(List<MenuItem> menuItems) {
+
+        for (int i = 0; i < menuItems.size(); i++) {
+            MenuItem menuItem = menuItems.get(i);
+            MenuItemView menuItemView = new MenuItemView(menuItem);
+            MenuGrid.add(menuItemView, i % 3, i / 3);
+            //System.out.println(1%3);
+            //System.out.println(1/3);
+            //AddtoOrder = new Button("Add to Order");
+
+        }
+    }
+
 }
